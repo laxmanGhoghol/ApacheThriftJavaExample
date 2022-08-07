e159456b27054967e0a5744b1fa58f3d70ec0137
Lakhabhai Ghoghol <lakhabhai.ghoghol@techverito.com>
2022-08-07 11:31:51 +0530

    add: test and threadPoolServer

--- src/main/java/com/laxman/practspringthrift/PractSpringThriftApplication.java
+++ src/main/java/com/laxman/practspringthrift/PractSpringThriftApplication.java
@@ -10,32 +10,26 @@ import org.apache.thrift.transport.TTransportException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 @Slf4j
 public class PractSpringThriftApplication {
     public static void main(String[] args) {
-		int port = 8082;
+        int port = 8082;
         SpringApplication.run(PractSpringThriftApplication.class, args);
-		Runnable server = new Runnable() {
-			@Override
-			public void run() {
-				start(port);
-			}
-		};
-		new Thread(server).start();
-		log.info("Started the server at port: " + port);
+        Runnable server = () -> start(port);
+        new Thread(server).start();
+        log.info("Started the server at port: " + port);
     }
-	public static void start(int port){
-		TServerTransport serverTransport;
-		try {
-			serverTransport = new TServerSocket(port);
-		} catch (TTransportException e) {
-			throw new IllegalStateException("Failed to start the server");
-		}
-		GreetingService.Processor<GreetingServiceImpl> processor = new GreetingService.Processor<>(new GreetingServiceImpl());
-		TServer server = new TSimpleServer(new TSimpleServer.Args(serverTransport).processor(processor));
-		server.serve();
-	}
+
+    public static void start(int port) {
+        TServerTransport serverTransport;
+        try {
+            serverTransport = new TServerSocket(port);
+        } catch (TTransportException e) {
+            throw new IllegalStateException("Failed to start the server");
+        }
+        GreetingService.Processor<GreetingServiceImpl> processor = new GreetingService.Processor<>(new GreetingServiceImpl());
+        TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+        server.serve();
+    }
 }
--- src/test/java/com/laxman/practspringthrift/PractSpringThriftApplicationTests.java
+++ src/test/java/com/laxman/practspringthrift/PractSpringThriftApplicationTests.java
@@ -1,13 +1,42 @@
 package com.laxman.practspringthrift;
 
-import org.junit.jupiter.api.Test;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PractSpringThriftApplicationTests {
+	private TTransport transport;
+	TProtocol protocol;
+	GreetingService.Client client;
+
+	@BeforeAll
+	void setUp() throws TTransportException {
+		transport = new TSocket("localhost", 8082);
+		protocol = new TBinaryProtocol(transport);
+		client = new GreetingService.Client(protocol);
+	}
+
+	@AfterAll
+	void tearDown(){
+		transport.close();
+	}
+
 
 	@Test
-	void contextLoads() {
+	void shouldReturnTrueWhenPingGreetService() throws TException, InterruptedException {
+		Thread.sleep(3000);
+		transport.open();
+		Assertions.assertTrue(client.ping());
 	}
 
 }
